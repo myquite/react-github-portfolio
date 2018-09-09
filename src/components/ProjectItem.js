2c8f8ae671eefd2a7d96dff008041eb1c5240de4
Mike White <myquite@gmail.com>
2018-09-09 18:27:37 -0500

    added data to the ProjectItem component

--- src/components/ProjectItem.js
+++ src/components/ProjectItem.js
@@ -2,8 +2,16 @@ import React from "react";
 
 const ProjectItem = (props) => {
   return (
-    <li>{props.project.name}</li>
+    <li>
+      <div>
+      
+        <h3>{props.project.name}</h3>
+        <p>{props.project.description}</p>
+      
+      </div>
+    </li>
   );
 };
+
 
 export default ProjectItem;
