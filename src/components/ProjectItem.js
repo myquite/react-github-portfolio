0b9a8d902cbcdcbb7e127f60fa33856792b6c545
Mike White <myquite@gmail.com>
2018-09-11 15:41:20 -0500

    Added screenshots to the ProjectItem components

--- src/components/ProjectItem.js
+++ src/components/ProjectItem.js
@@ -5,6 +5,7 @@ const ProjectItem = (props) => {
     <li>
       <div>
         <h3>{props.project.name}</h3>
+        <img src={`https://raw.githubusercontent.com/${props.project.full_name}/master/${props.project.name}.png`} alt="project screenshot"/>
         <p>{props.project.description}</p>
         <a href={props.project.html_url} target="_blank">Github Link</a>
       </div>
