80f0c777f00f5f471aa69999dbb26019224234b4
Mike White <myquite@gmail.com>
2018-09-12 16:10:31 -0500

    playing with the design a bit and renamed login to username in App state

--- src/App.js
+++ src/App.js
@@ -8,20 +8,20 @@ class App extends Component {
     super(props);
 
     this.state = {
-      login: "myquite", 
+      username: "myquite", 
       user: [],
       repos: []
     };
   };
 
   componentDidMount() {
-    fetch(`https://api.github.com/users/${this.state.login}`)
+    fetch(`https://api.github.com/users/${this.state.username}`)
     .then(res => res.json())
     .then((user) => {
       this.setState({ user });
     });
 
-    fetch(`https://api.github.com/users/${this.state.login}/repos`)
+    fetch(`https://api.github.com/users/${this.state.username}/repos`)
     .then(res => res.json())
     .then((repos) => {
       this.setState({ repos });
@@ -32,7 +32,7 @@ class App extends Component {
     return (
       <section className="App">
         <UserInfo user={this.state.user}/>
-        <ProjectList repos={this.state.repos} login={this.state.login}/>
+        <ProjectList repos={this.state.repos} username={this.state.username}/>
       </section>
     );
   };
