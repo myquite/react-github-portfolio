fbd2261e2b9497765c389d3da354b1225c1f58db
Mike White <myquite@gmail.com>
2018-09-13 15:48:42 -0500

    converted ProjectList back to a functional component

--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -1,22 +1,20 @@
-import React, { Component } from "react";
+import React from "react";
 import '../App.css';
 import ProjectItem from "./ProjectItem";
 
 
-class ProjectList extends Component {
-  render() {
-    return (
-      <section className="projects">
-        <ul> 
-          {
-            this.props.repos.map((project) => {
-              return <ProjectItem key={project.id} project={project} />
-            })
-          }
-        </ul>
-      </section>
-    );
-  };
+const ProjectList = (props) => {
+  return (
+    <section className="projects">
+      <ul> 
+        {
+          props.repos.map((project) => {
+            return <ProjectItem key={project.id} project={project} />
+          })
+        }
+      </ul>
+    </section>
+  );
 };
 
 export default ProjectList;
